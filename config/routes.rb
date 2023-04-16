c71a09d6dc0edc6924f82ee401675afb014be43c
jingleville <andrewgavrick@yandex.ru>
2023-04-16 11:19:29 +0300

    finished lesson 38

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :articles
-  resource :contacts, only: [:new, :create, :show]
+  resource :contacts, only: [:new, :create, :show], path_names: { new: '' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
